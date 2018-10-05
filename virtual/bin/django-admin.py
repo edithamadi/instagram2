80b825b4a05d1262e4584f596f73c683c89859b7
Edith <you@example.com>
2018-10-05 11:32:03 +0300

    initialised files and created views file

---
+++ virtual/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/edith/Documents/core/django/django_projects/instagram2/virtual/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
