5d09332c696d5797264807f08669599b575e3f85
Edith <you@example.com>
2018-10-05 15:39:25 +0300

    created image class

--- insta/models.py
+++ insta/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Image(models.Model):
+   image = models.ImageField(upload_to='images/')
+   image_name = models.CharField(max_length=40)
+   image_caption = models.TextField()
+   likes = models.PositiveIntegerField(default=0)
+   comments = models.CharField(max_length=200)
+   profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+   
