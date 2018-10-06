128987622a37df73d984df4d1e4396ce3a9c3dad
Edith <you@example.com>
2018-10-06 12:50:05 +0300

    added models to admin page

--- insta/admin.py
+++ insta/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
 # Register your models here.
+from .models import Image,Profile
+
+class ImageAdmin(admin.ModelAdmin):
+    filter_horizontal =('comments',)
+
+
+admin.site.register(Image)
+admin.site.register(Profile)
