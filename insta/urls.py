60dd5043dabc9486440d274805e62d597cba3618
Edith <you@example.com>
2018-10-11 10:52:59 +0300

    added urls for profile editing

--- insta/urls.py
+++ insta/urls.py
@@ -5,9 +5,11 @@ from django.conf.urls.static import static
 
 from django.conf import settings
 urlpatterns=[
-    url('^$',views.welcome,name = 'welcome'),
-    url(r'^new/image$', views.new_img, name = 'new_img'),
-    url(r'^search/', views.search_profile, name='search_profile')
+    url(r'^$',views.welcome,name = 'welcome'),
+    url(r'^new/image/$', views.new_img, name = 'new_img'),
+    url(r'^search/', views.search_profile, name='search_profile'),
+    url(r'^(?P<user_username>\w+)/$' , views.profile , name= 'profile'),
+    url(r'^(?P<user_username>\w+)/edit$' , views.profile , name= 'edit_profile'),
 ]
 if settings.DEBUG:
    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
