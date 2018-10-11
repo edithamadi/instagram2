fbe8a57ec4823d1ca90e4ef2fecc88d494e92a23
Edith <you@example.com>
2018-10-11 10:49:35 +0300

    added profile form

--- insta/forms.py
+++ insta/forms.py
@@ -1,6 +1,11 @@
 from django import forms
-from .models import Image
+from .models import Image,Profile
 class InstaForm(forms.ModelForm):
     class Meta:
-        model=Image
+        model = Image
         exclude = ['likes', 'comment']
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ('profile_photo', 'bio', 'first_name')
+        exclude = ['user', 'last_name']
